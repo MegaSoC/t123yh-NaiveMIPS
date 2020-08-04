5d85caf897ee68de9412c9a3d68e2c63157c7771
shh2000 <13820618441@163.com>
2020-08-04 18:18:34 +0800

    fix stall

--- myCPU/src/core/AT.sv
+++ myCPU/src/core/AT.sv
@@ -5,7 +5,6 @@ module AT(
         input [4:0] Rs,
         input [4:0] Rt,
         input [3:0] D_T,
-        input D_WriteRegEnable,
         input [4:0] D_RegId,
         input [3:0] E_T,
         input E_WriteRegEnable,
@@ -25,26 +24,24 @@ module AT(
     wire NeedRt         =(add||addu||sub||subu||sw||swl||swr||sb||sh||mult||multu||div||divu||sll||srl||sra||sllv||srlv||srav||
                   my_And||my_Or||my_Xor||my_Nor||slt||sltu||beq||bne||mtc0||mul);
 
-    wire [3:0] T_Rs     =(beq||bne||blez||bgtz||bltz||bgez||bltzal||bgezal) ? 0:
+    wire [3:0] T_Rs     =
          (addi||addiu||add||addu||sub||subu||lw||lwl||lwr||sw||swl||swr||lb||lbu||lh||lhu||sb||sh||mult||multu||div||mthi||mtlo||
           divu||sllv||srlv||srav||my_And||my_Or||my_Xor||my_Nor||ori||Andi||Xori||slt||sltu||slti||sltiu||mul) ? 1:0;
 
-    wire [3:0] T_Rt     =(beq||bne) ? 0:
+    wire [3:0] T_Rt     =
          (add||addu||sub||subu||mult||multu||div||divu||sll||srl||sra||sllv||srlv||srav||
           my_And||my_Or||my_Xor||my_Nor||slt||sltu||mul) ? 1:
          (sw||swl||swr||sb||sh||mtc0) ? 2:0;
 
-    wire [3:0] Tnew     =(lw||lhu||lh||lbu||lb||mfc0) ? 3:
+    assign T            =
          (addi||addiu||add||addu||sub||subu||lui||jal||jalr||mfhi||mflo||sll||srl||sra||sllv||srlv||srav||
           my_And||my_Or||my_Xor||my_Nor||ori||Andi||Xori||slt||sltu||slti||sltiu) ? 2:
          (lwl|lwr) ? 4:
          3;
-    assign T            = Tnew;
     wire MultFamily;
     assign MultFamily   = (mult|multu|div|divu|mfhi|mflo|mthi|mtlo|mul);
-    wire Rt_stallByD    = (D_WriteRegEnable && D_T>T_Rt && D_RegId==Rt);
-    wire stall_Rs       = NeedRs && Rs!=5'b0 &&( (D_WriteRegEnable && D_T>T_Rs && D_RegId==Rs)|| (Rs!=5'd0 && E_T>T_Rs && E_RegId==Rs)),
-         stall_Rt       = NeedRt && Rt!=5'b0 &&( Rt_stallByD|| (Rt!=5'd0 && E_T>T_Rt && E_RegId==Rt));
+    wire stall_Rs       = NeedRs && Rs!=5'b0 &&( (D_T>T_Rs && D_RegId==Rs)|| (E_T>T_Rs && E_RegId==Rs)),
+         stall_Rt       = NeedRt && Rt!=5'b0 &&( (D_T>T_Rt && D_RegId==Rt)|| (E_T>T_Rt && E_RegId==Rt));
 
     wire stall_XALU;
     assign stall_XALU   = ((XALU_Busy&MultFamily)|(MultFamily&D_MultCalFamily));
