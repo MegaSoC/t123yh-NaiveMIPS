2e292e48ebf65b873524ab7e1cde3c18b3499fe9
shh2000 <13820618441@163.com>
2020-08-04 12:26:01 +0800

    fix lwl

--- myCPU/src/core/MemExtUnit.sv
+++ myCPU/src/core/MemExtUnit.sv
@@ -10,18 +10,21 @@ module MemExtUnit(
     );
     wire lb,lbu,lh,lhu,lw,lwl,lwr,swl,swr;
     assign {lb,lbu,lh,lhu,lw,lwl,lwr,swl,swr}=ExtType;
-    wire [31:0] OffsetedData = lwl ? ( RawMemData<<({Offset,3'b0})):
-         ( RawMemData>>({Offset,3'b0}));
+    logic [31:0] lwld,alwld;
+    assign lwld = ( RawMemData<<({Offset,3'b0}));
+    assign alwld = ( RawMemData>>({Offset,3'b0}));
 
     logic normal;
-    assign normal = !(lb|lbu|lh|lhu);
-    assign ExtMemData = ({32{lhu}} & {16'b0,OffsetedData[15:0]})|
-           ({32{lh}} & {{16{OffsetedData[15]}},OffsetedData[15:0]})|
-           ({32{lbu}} & {24'b0,OffsetedData[7:0]})|
-           ({32{lb}} & {{24{OffsetedData[7]}},OffsetedData[7:0]})|
-           ({32{normal}} & OffsetedData);
+    assign normal = !(lb|lbu|lh|lhu|lwl);
+    assign ExtMemData = ({32{lhu}} & {16'b0,alwld[15:0]})|
+           ({32{lh}} & {{16{alwld[15]}},alwld[15:0]})|
+           ({32{lbu}} & {24'b0,alwld[7:0]})|
+           ({32{lb}} & {{24{alwld[7]}},alwld[7:0]})|
+           ({32{lwl}} & lwld)|
+           ({32{normal}} & alwld);
     wire [3:0] WritePreExted = {4{M_WriteRegEnable}};
-    assign M_WriteRegEnableExted = lwr ? WritePreExted>>Offset:
-           lwl ? WritePreExted<<(~Offset):
-           WritePreExted;
+    logic pnormal;
+    assign pnormal = !(lwl|lwr);
+    assign M_WriteRegEnableExted = ({32{lwr}}&(WritePreExted>>Offset)) | ({32{lwl}}&( WritePreExted<<(~Offset))) | ({32{pnormal}}&(WritePreExted));
+
 endmodule
