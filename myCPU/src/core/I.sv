406c34bd1ceda2444f9b3277941b8b4883d04881
shh2000 <13820618441@163.com>
2020-08-11 21:45:29 +0800

    fix I logic

--- myCPU/src/core/I.sv
+++ myCPU/src/core/I.sv
@@ -1,7 +1,5 @@
 `timescale 1ns / 1ps
 `define TextAddr (32'hbfc00000)
-`define IDLE 2'b01
-`define FETCH 2'b10
 
 module I(
         input clk,
@@ -35,42 +33,43 @@ module I(
     reg [31:0] pcReg,pcReg2;
     assign iPcWire = pcReg;
     assign iPcWire2 = pcReg2;
-    wire [1:0] nextState ;
 
-
-    always_ff @ (posedge clk) begin
-        if (reset | expFlush ) begin
+    always_ff @ (posedge clk)begin
+        if(reset)begin
             iPcReg <= 0;
             iInstr <= 0;
             iInstMiss <= 0;
             iInstIllegal <= 0;
             iInstInvalid <= 0;
-        end
-        else if (nextState==`FETCH) begin
-            iPcReg <= pcReg;
-            iInstr <=(instUncached)? instSramData: iIcacheRdata;
-            iInstMiss <= instMiss;
-            iInstIllegal <= instIllegal;
-            iInstInvalid <= instInvalid;
-        end
-        if(reset) begin
             pcReg<=`TextAddr;
             pcReg2<=`TextAddr;
         end
-
-        else if (dStall |dmStall ) begin
-            pcReg<=pcReg;
-            pcReg2<=pcReg2;
+        else if(flush)begin
+            iPcReg <= 0;
+            iInstr <= 0;
+            iInstMiss <= 0;
+            iInstIllegal <= 0;
+            iInstInvalid <= 0;
+            if  (!dStall & !dmStall & ((instUncached & instSramValid ) | (!instUncached & !icacheStall ))) begin
+                pcReg<=dNpc;
+                pcReg2<=dNpc;
+            end
         end
-        else if (nextState==`FETCH) begin
-            pcReg<=dNpc;
-            pcReg2<=dNpc;
+        else begin
+            if  (!dStall & !dmStall & ((instUncached & instSramValid ) | (!instUncached & !icacheStall ))) begin
+                pcReg<=dNpc;
+                pcReg2<=dNpc;
+                iPcReg <= pcReg;
+                iInstr <=(instUncached)? instSramData: iIcacheRdata;
+                iInstMiss <= instMiss;
+                iInstIllegal <= instIllegal;
+                iInstInvalid <= instInvalid;
+            end
         end
     end
 
 
     assign iNextNotReady = (dStall | dmStall | ((!instUncached | !instSramValid ) & (instUncached | icacheStall ))) ;
-    assign nextState = (!dStall & !dmStall & ((instUncached & instSramValid ) | (!instUncached & !icacheStall ))) ? `FETCH : `IDLE ;
 
 endmodule
 
