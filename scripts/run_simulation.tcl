52782015ba4638ef81616c376cc854905d0364f3
Yunhao Tian <t123yh@outlook.com>
2021-02-01 16:12:03 +0800

    Try to speed up simulation

--- scripts/run_simulation.tcl
+++ scripts/run_simulation.tcl
@@ -1,6 +1,9 @@
 source [file join [file dirname [info script]] "import_files.tcl"]
 update_compile_order -fileset sources_1
 
+set_property -name {xsim.elaborate.debug_level} -value off  -objects [get_filesets sim_1]
+set_property -name {xsim.simulate.log_all_signals} -value false  -objects [get_filesets sim_1]
+
 generate_target simulation [get_ips *]
 set simulations [get_fileset $env(SIMULATION)]
 
