1ce9919a0926dcb077862cba38682246c23e7433
Yunhao Tian <t123yh@outlook.com>
2021-04-14 00:55:32 +0000

    Remove assertions

--- myCPU/src/memory_management/cache_mem.sv
+++ myCPU/src/memory_management/cache_mem.sv
@@ -36,8 +36,10 @@ end
 
 generate
 if (C_ASIC_SRAM) begin
+/*
     always assert ((1 << INDEX_WIDTH) == 1024) else
         $fatal("data_ram memory depth mismatch, should be 1024!");
+*/
     
     S018DP_RAM_DP_W1024_B32_M4_BW data_mem(
       .CLKA(i_clk),
@@ -149,11 +151,13 @@ end
 
 generate
 if (C_ASIC_SRAM) begin
+/*
     always assert ((1 << INDEX_WIDTH) == 64) else
         $fatal("tag_ram memory depth mismatch, should be 64!");
 
     always assert ((TAG_WIDTH + 1) == 21) else
         $fatal("tag_ram memory width mismatch, should be 21!");
+*/
     
     S018DP_RAM_DP_W64_B21_M4 data_mem (
         .CLKA(i_clk),
